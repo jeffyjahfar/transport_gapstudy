2f1ce4d65f7751b4800fd9b4d94766ab076e33f5
Jeffy Jahfar <jeffyjahfar@gmail.com>
2014-03-19 03:27:00 +0530

    backup 19 march

---
+++ backup/19-03-14/SaturationFlow_ace28aug2013/src/main/pdfFilefilter.java
@@ -0,0 +1,21 @@
+package main;
+
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+public class pdfFilefilter extends FileFilter
+{
+/* accept directories and files with ".pdf" extension */
+public boolean accept(File f)
+{
+return f.getName().toLowerCase().endsWith(".pdf")
+| f.isDirectory();
+}
+/* description of file type */
+public String getDescription()
+{
+return "Portable Document Format (.pdf)";
+}
+}
